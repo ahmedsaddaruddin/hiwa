7e97e89bed5e92e4168b3eb5b17f48d690768c20
ahmedsaddaruddin <49924020+ahmedsaddaruddin@users.noreply.github.com>
2019-05-12 11:49:35 -0400

    Update customers.php

--- php/customers.php
+++ php/customers.php
@@ -8,15 +8,18 @@ if (!array_key_exists('hiwa-user', $_COOKIE) ||
 	exit();
 }
 
-$role=$_COOKIE['hiwa-role'];
+$role=$_SESSION['hiwa-role'];
 
 if (array_key_exists('action', $_REQUEST) &&
     array_key_exists('custid', $_REQUEST) &&
     $_REQUEST['action'] == 'delete') {
 	$conn = pg_connect('user='.$CONFIG['username'].
 		' dbname='.$CONFIG['database']);
-	$res = pg_query($conn, "DELETE FROM customers WHERE 
+	
+	$res = pg_prepare($conn,"delete_query" "DELETE FROM customers WHERE 
 		customerid='".$_REQUEST['custid']."'");
+	$res = pg_execute($conn, "delete_query")
+		
 	if ($res === False) {
 		$msg = "Unable to remove customer";
 	}
@@ -29,14 +32,15 @@ else if (array_key_exists('custid', $_REQUEST) &&
 
 	$conn = pg_connect('user='.$CONFIG['username'].
 		' dbname='.$CONFIG['database']);
-	$res = pg_query($conn, "INSERT INTO customers
+	$res = pg_prepare($conn,"insert_query" "INSERT INTO customers
 		(customerid, customername, creditlimit, taxid)
 		VALUES
 		('".$_REQUEST['custid']."', '".
 		$_REQUEST['custname']."', ".
 		$_REQUEST['limit'].", '".
 		$_REQUEST['taxid']."')");
-	if ($res === False) {
+		$res = pg_execute($conn, "insert_query")
+		if ($res === False) {
 		$msg="Unable to create customer.";
 	}
 }
@@ -51,7 +55,8 @@ else if (array_key_exists('custid', $_REQUEST) &&
 <body>
 <?php require 'header.php';?>
 <div class="title">HIWA Manage Customers</div>
-<div class="subtitle">Logged in as <?php echo $_COOKIE['hiwa-user'];?>
+<div class="subtitle">Logged in as 
+	<?php echo $_SESSION['hiwa-user'];?>
 	(<?php echo $role; ?>)
 </div>
 
